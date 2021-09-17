4b0e87fb8c1eb7c58d4e205df2b025cdeb262bd3
huy vu~ <sivin.nguyen@gmail.com>
2021-09-17 18:39:30 +0700

    fix bug

--- client/js/socket.js
+++ client/js/socket.js
@@ -1,41 +1,39 @@
 'use strict'
 
-window.addEventListener('DOMContentLoaded', (event) => {
-    const origin = location.origin.replace('http', 'ws')
-    const WS_URL = `${origin}/ws`
+const origin = location.origin.replace('http', 'ws')
+const WS_URL = `${origin}/ws`
 
-    const socket = new WebSocket(WS_URL)
+const socket = new WebSocket(WS_URL)
 
-    socket.onopen = function (e) {
-        console.log('[open] Connection established!')
+socket.onopen = function (e) {
+    console.log('[open] Connection established!')
+}
+
+socket.onclose = function (e) {
+    console.log(e)
+    if(e.code == 4001) {
+        console.log(e.reason)
     }
 
-    socket.onclose = function (e) {
-        console.log(e)
-        if(e.code == 4001) {
-            console.log(e.reason)
-        }
-
-        if(e.code = 1006) {
-            console.log('[server] shut down! ')
-        }
-
-        console.log('[close] Disconnected!')
+    if(e.code == 1006) {
+        console.log('[server] shut down! ')
     }
 
-    socket.onerror = function (e) {
-        console.log(e)
-    }
+    console.log('[close] Disconnected!')
+}
 
-    socket.onmessage = function (message) {
-        console.log(message.data)
-    }
+socket.onerror = function (e) {
+    console.log(e)
+}
 
-    // Close connection when tab closed
-    // https://stackoverflow.com/questions/4812686/closing-websocket-correctly-html5-javascript
-    window.addEventListener('unload', function(e) {
-        if(socket.readyState == WebSocket.OPEN) {
-            socket.close(1001)
-        }
-    })
+socket.onmessage = function (message) {
+    console.log(message.data)
+}
+
+// Close connection when tab closed
+// https://stackoverflow.com/questions/4812686/closing-websocket-correctly-html5-javascript
+window.addEventListener('unload', function(e) {
+    if(socket.readyState == WebSocket.OPEN) {
+        socket.close(1001)
+    }
 })
